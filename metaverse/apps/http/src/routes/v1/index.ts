1eb0ee30d5d2e1adb1c89acf85cd9bbe0f822753
Nevil Patel <npatel@fleetoperate.com>
2025-01-08 01:12:34 -0500

    Fix: Types errors, Feat: added working Testing and database

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -2,28 +2,38 @@ import { Router } from "express";
 import { userRouter } from "./users";
 import { adminRouter } from "./admin";
 import { spaceRouter } from "./space";
-import { SignupSchema } from "src/types";
+import { SigninSchema, SignupSchema } from "src/types";
 import client from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import {hash, compare} from "../../scrypt";
+import { JWT_PASSWORD } from "src/config";
 
 
 export const router = Router();
-router.post("/signup",(req, res) => {
+router.post("/signup",async (req, res) => {
     const parsedData = SignupSchema.safeParse(req.body)
     if(!parsedData.success){
         res.status(400).json({message: "Validation failed"})
         return
         }
 
-    try {
+        if (!parsedData.data.password) {
+            res.status(400).json({ message: "Password is required" });
+            return;
+        }
+    
+
+        const hashedPassword = await hash(parsedData.data.password)
+        try {
         const user = client.user.create({
             data: {
             username: parsedData.data.username,
-            password: parsedData.data.password,
+            password: hashedPassword,
             role: parsedData.data.type === "admin" ? "Admin" : "User",
             }
         })
         res.json({
-            userId: user.id
+            userId: (await user).id // This line await might cause an error
         })
     } catch (error) {
         res.status(400).json({message: "User already exist"})
@@ -32,10 +42,49 @@ router.post("/signup",(req, res) => {
     
 })
 
-router.post("/signin",(req, res) => {
-    res.json({
-        message: "Signin"
-    })
+router.post("/signin",async (req, res) => {
+
+    const parsedData = SigninSchema.safeParse(req.body)
+    if(!parsedData.success){
+        res.status(403).json({message: "Validation Failed"})
+    }
+
+    if (!parsedData.data?.username || !parsedData.data.password) {
+        res.status(403).json({ message: "Username and password are required" });
+        return;
+    }
+
+    try{
+        const user = await client.user.findUnique({
+            where: {
+                username: parsedData.data?.username
+            }
+        })
+        if(!user){
+            res.status(403).json({
+                message: "User not found"
+            })
+            return
+        }
+        const isValid = await compare(parsedData.data?.password, user.password)
+        if(!isValid){
+            res.json(403).json({
+                message: "Invalid Password"
+            })
+            return
+        }
+
+        const token = jwt.sign({
+            userId: user.id,
+            role: user.role,
+        }, JWT_PASSWORD);
+
+        res.json({
+            token
+        })
+    } catch(e){
+        res.status(400).json({message: "Internal server error"})
+    }
 })
 
 router.get("/elements", (req,res) => {
--- metaverse/apps/http/src/routes/v1/users.ts
+++ metaverse/apps/http/src/routes/v1/users.ts
@@ -1,16 +1,50 @@
 import { Router } from "express";
+import { UpdateMetadataSchema } from "src/types";
+import client from "@repo/db"
+import { userMiddleware } from "src/middleware/user";
 
 export const userRouter = Router();
 
 
-userRouter.post("/metadata", (req, res) => {
+userRouter.post("/metadata", userMiddleware, async (req, res) => {
+    const parsedData = UpdateMetadataSchema.safeParse(req.body)
+    if(!parsedData.success){
+        res.status(400).json({mesaage: "Validation Failed"})
+        return
+    }
+    await client.user.update({
+        where: {
+            id: req.userId
+        },
+        data: {
+            avatarId: parsedData.data.avatarId
+        }
+    
+    })
     res.json({
-        message: ""
+        message: "Metadata updated"
     })
 })
 
-userRouter.get("/api/v1/user/metadata/bulk", (req,res) => { 
+userRouter.get("/metadata/bulk", async (req, res) => {
+    const userIdString = (req.query.ids ?? "[]") as string;
+    const userIds = (userIdString).slice(1, userIdString?.length - 1).split(",");
+    console.log(userIds)
+    const metadata = await client.user.findMany({
+        where: {
+            id: {
+                in: userIds
+            }
+        }, select: {
+            avatar: true,
+            id: true
+        }
+    })
+
     res.json({
-        message: ""
+        avatars: metadata.map(m => ({
+            userId: m.id,
+            avatarId: m.avatar?.imageUrl
+        }))
     })
 })
