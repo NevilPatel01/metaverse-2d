a6faae1466bc751f435aaf3bef0ac1615350b8c4
Nevil Patel <npatel@fleetoperate.com>
2025-01-21 12:31:13 -0500

    Fix: Websocket implementation

--- metaverse/apps/ws/src/index.ts
+++ metaverse/apps/ws/src/index.ts
@@ -1,6 +1,6 @@
 import {WebSocketServer} from 'ws';
 
-const wss = new WebSocket({ port : 3001 });
+const wss = new WebSocketServer({ port : 3001 });
 
 wss.on('connection', function connection(ws){
     ws.on('error', console.error);
